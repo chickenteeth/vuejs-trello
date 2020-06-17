d453463373a25f87bcc3fb9d2aed673db93aee92
Tristan Stutzman <tristan.stutzman@gmail.com>
2020-06-17 14:38:36 -0700

    installed VueJS, added card and list structure and functionaliity

---
+++ app/javascript/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="row">
+    <div v-bind:key="list" v-for="list in original_lists" class="col-3">
+      <h5>{{ list.name }}</h5>
+      <hr />
+
+      <div
+        v-bind:key="(card, index)"
+        v-for="(card, index) in list.cards"
+        class
+        card
+        card-body
+      >{{ card.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["original_lists"],
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
